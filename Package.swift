655c5bc19d8605bad2e6ddd9d5142346cf0d9922
fto-k <fto@kovalee.app>
2023-04-21 16:21:56 +0200

    new framework version

--- Package.swift
+++ Package.swift
@@ -13,8 +13,6 @@ let package = Package(
             name: "KovaleeSDK",
             targets: [
                 "KovaleeFramework",
-//                "Adjust",
-//                "FrameworkWrapper"
             ]
         ),
     ],
@@ -27,19 +25,5 @@ let package = Package(
             name: "KovaleeFramework",
             path: "./Sources/KovaleeFramework.xcframework"
         ),
-//        .target(
-//            name: "FrameworkWrapper",
-//            dependencies: [
-//                "KovaleeFramework",
-////                "Adjust"
-//                .product(name: "Adjust", package: "ios_sdk")
-//            ]
-////            path: "FrameworkWrapper"
-//        ),
-//        .binaryTarget(
-//            name: "Adjust",
-//            url: "https://github.com/adjust/ios_sdk/releases/download/v4.33.4/AdjustSdk-iOS-tvOS-Dynamic-4.33.4.xcframework.zip",
-//            checksum: "67366734fdce1aee75fbcebe71379177e98ce1645dd1694d10a8f845815ab4f9"
-//        )
     ]
 )
