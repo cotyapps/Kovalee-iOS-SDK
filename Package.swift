5a2a46aff496bf07a728bea73bd3e18238aded8a
fto-k <fto@kovalee.app>
2023-07-27 15:15:16 +0200

    feat: moved Purchases to specific package

--- Package.swift
+++ Package.swift
@@ -21,18 +21,11 @@ let package = Package(
 			targets: [
 				"KovaleeAttribution",
 			]
-		),
-		.library(
-			name: "KovaleePurchases",
-			targets: [
-				"KovaleePurchases",
-			]
 		)
     ],
     dependencies: [
 		.package(url: "https://github.com/adjust/ios_sdk", from: Version(4, 33, 5)),
-		.package(url: "https://github.com/amplitude/Amplitude-Swift", from: Version(0, 4, 6)),
-		.package(url: "https://github.com/RevenueCat/purchases-ios", from: Version(4, 25, 0)),
+		.package(url: "https://github.com/amplitude/Amplitude-Swift", from: Version(0, 4, 6))
 	],
     targets: [
         .binaryTarget(
@@ -53,13 +46,6 @@ let package = Package(
 				"KovaleeSDK",
 				.product(name: "Adjust", package: "ios_sdk")
 			]
-		),
-		.target(
-			name: "KovaleePurchases",
-			dependencies: [
-				"KovaleeSDK",
-				.product(name: "RevenueCat", package: "purchases-ios")
-			]
-		),
+		)
     ]
 )
