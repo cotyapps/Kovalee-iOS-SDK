78f32b374e4380787422591318db8222d1908d50
fto-k <fto@kovalee.app>
2024-01-26 16:24:53 +0100

    feat: updated UI

--- Sources/KovaleeSDKUI/DebugView.swift
+++ Sources/KovaleeSDKUI/DebugView.swift
@@ -4,36 +4,74 @@ import SwiftUI
 
 public struct DebugView: View {
     @State private var abTestValue: String?
-    @State private var customerInfo: String?
+    @State private var customerSubscriptions: Set<String>?
 
     public var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                Text("SDK Initialized: \(Kovalee.isInitialized ? "✅" : "❌")")
-                Text("Data collection enabled: \(Kovalee.isDataCollectionEnabled() ? "✅" : "❌")")
+            List {
+                HStack {
+                    infoLabel(
+                        title: "SDK Initialized:",
+                        value: Kovalee.isInitialized ? "✅" : "❌"
+                    )
+                }
+                HStack {
+                    infoLabel(
+                        title: "Data collection enabled:",
+                        value: Kovalee.isDataCollectionEnabled() ? "✅" : "❌"
+                    )
+                }
                 if let userId = Kovalee.getAmplitudeUserId() {
-                    Text("Amplitude User Id: \(userId)")
+                    infoLabel(
+                        title: "Amplitude User Id:",
+                        value: userId,
+                        horizontal: false
+                    )
                 }
                 if let deviceId = Kovalee.shared.kovaleeManager?.amplitudeDeviceId() {
-                    Text("Amplitude Device Id: \(deviceId)")
+                    infoLabel(
+                        title: "Amplitude Device Id:",
+                        value: deviceId,
+                        horizontal: false
+                    )
                 }
                 if let adid = Kovalee.shared.kovaleeManager?.getAttributionAdid() {
-                    Text("User ADID: \(adid)")
+                    infoLabel(title: "User ADID:", value: adid, horizontal: false)
                 }
                 if let abTestValue {
-                    Text("AB test Value: \(abTestValue)")
+                    infoLabel(title: "AB test Value:", value: abTestValue)
                 }
 
-                if let customerInfo {
-                    Text("CustomerInfo: \(customerInfo)")
+                if let customerSubscriptions {
+                    VStack(alignment: .leading) {
+                        Text("Customer Subscriptions:").bold()
+                        ForEach(Array(customerSubscriptions), id: \.self) {
+                            Text($0)
+                        }
+                    }
                 }
             }
             .navigationTitle("SDK Debug View")
             .onAppear {
                 Task {
                     self.abTestValue = await Kovalee.shared.kovaleeManager?.abTestValue(forKey: "ab_test_version")
-                    self.customerInfo = try? await Kovalee.shared.kovaleeManager?.customerInfo().debugDescription
+                    self.customerSubscriptions = try? await Kovalee.shared.kovaleeManager?.customerInfo()?.activeSubscriptions
                 }
+            }
+        }
+    }
+
+    @ViewBuilder
+    func infoLabel(title: String, value: String, horizontal: Bool = true) -> some View {
+        if horizontal {
+            HStack {
+                Text(title).bold()
+                Text(value)
+            }
+        } else {
+            VStack(alignment: .leading) {
+                Text(title).bold()
+                Text(value)
             }
         }
     }
