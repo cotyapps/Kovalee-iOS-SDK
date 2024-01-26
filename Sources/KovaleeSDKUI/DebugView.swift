49f56e28d742dba8e54af676030a21faae21ec8f
fto-k <fto@kovalee.app>
2024-01-26 16:02:44 +0100

    feat: added a few data point to DebugView

--- Sources/KovaleeSDKUI/DebugView.swift
+++ Sources/KovaleeSDKUI/DebugView.swift
@@ -1,8 +1,41 @@
+import KovaleeFramework
+import KovaleeSDK
 import SwiftUI
 
 public struct DebugView: View {
+    @State private var abTestValue: String?
+    @State private var customerInfo: String?
+
     public var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("SDK Initialized: \(Kovalee.isInitialized ? "✅" : "❌")")
+                Text("Data collection enabled: \(Kovalee.isDataCollectionEnabled() ? "✅" : "❌")")
+                if let userId = Kovalee.getAmplitudeUserId() {
+                    Text("Amplitude User Id: \(userId)")
+                }
+                if let deviceId = Kovalee.shared.kovaleeManager?.amplitudeDeviceId() {
+                    Text("Amplitude Device Id: \(deviceId)")
+                }
+                if let adid = Kovalee.shared.kovaleeManager?.getAttributionAdid() {
+                    Text("User ADID: \(adid)")
+                }
+                if let abTestValue {
+                    Text("AB test Value: \(abTestValue)")
+                }
+
+                if let customerInfo {
+                    Text("CustomerInfo: \(customerInfo)")
+                }
+            }
+            .navigationTitle("SDK Debug View")
+            .onAppear {
+                Task {
+                    self.abTestValue = await Kovalee.shared.kovaleeManager?.abTestValue(forKey: "ab_test_version")
+                    self.customerInfo = try? await Kovalee.shared.kovaleeManager?.customerInfo().debugDescription
+                }
+            }
+        }
     }
 }
 
