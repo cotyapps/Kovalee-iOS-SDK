5859a9cfd2851099a0bc71baac7843992fc09926
fto-k <fto@kovalee.app>
2024-05-02 11:22:34 +0200

    fix: fixed tracking authorization and related cv

--- Sources/KovaleeSDK/Kovalee.swift
+++ Sources/KovaleeSDK/Kovalee.swift
@@ -1,7 +1,7 @@
 import Foundation
 import KovaleeFramework
 
-public let SDK_VERSION = "1.9.7"
+public let SDK_VERSION = "1.9.8"
 
 /// A wrapper around all the third party tools used by Kovalee to gather information within the apps
 ///
