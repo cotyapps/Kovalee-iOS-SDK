5dcd04a92cb59fc7b808f3cc793d01fce0ce7972
fto-k <fto@kovalee.app>
2024-05-23 15:06:55 +0200

    fix: sync amplitudeId in RC

--- Sources/KovaleeSDK/Kovalee.swift
+++ Sources/KovaleeSDK/Kovalee.swift
@@ -1,7 +1,7 @@
 import Foundation
 import KovaleeFramework
 
-public let SDK_VERSION = "1.9.13"
+public let SDK_VERSION = "1.9.14"
 
 /// A wrapper around all the third party tools used by Kovalee to gather information within the apps
 ///
