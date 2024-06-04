52d23f7dbafa17087ad9e6ab3dfe29b3b4dfcafa
fto-k <fto@kovalee.app>
2024-06-04 11:46:08 +0200

    chore: cleaned up fastfile

--- Sources/KovaleeSDK/Kovalee.swift
+++ Sources/KovaleeSDK/Kovalee.swift
@@ -1,7 +1,7 @@
 import Foundation
 import KovaleeFramework
 
-public let SDK_VERSION = "1.9.18"
+public let SDK_VERSION = "1.9.19"
 
 /// A wrapper around all the third party tools used by Kovalee to gather information within the apps
 ///
