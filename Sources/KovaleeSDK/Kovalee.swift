f48d7e7d732fdc436fe8a2f84cec7cb7a0d083f5
fto-k <fto@kovalee.app>
2024-06-06 16:54:31 +0200

    chore: fastlane file update

--- Sources/KovaleeSDK/Kovalee.swift
+++ Sources/KovaleeSDK/Kovalee.swift
@@ -1,7 +1,7 @@
 import Foundation
 import KovaleeFramework
 
-public let SDK_VERSION = "1.9.20"
+public let SDK_VERSION = "1.9.21"
 
 /// A wrapper around all the third party tools used by Kovalee to gather information within the apps
 ///
