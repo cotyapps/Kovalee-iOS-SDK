1bf2c6c20ef25e3d69cfee57088e4759f4a4ed15
Rodrigo Giglio <rodrigofgiglio@gmail.com>
2024-06-28 11:04:01 +0200

    feat: implement methods on KovaleeSurveyManagerImpl

--- Sources/KovaleeSurvey/KovaleeSurveyManagerImpl.swift
+++ Sources/KovaleeSurvey/KovaleeSurveyManagerImpl.swift
@@ -4,8 +4,14 @@ import Foundation
 import KovaleeFramework
 
 class KovaleeSurveyManagerImpl: SurveyManager, Manager {
-    
+
+    private enum Constants {
+        static let amplitudeUserIdKey = "user_id"
+        static let pageViewEventNamePrefix = "page_view"
+    }
+
     private let survicate: SurvicateSdk?
+    weak var delegate: KovaleeSurveyDelegate?
 
     init(withKey workspaceKey: String?) {
 
@@ -26,8 +32,16 @@ class KovaleeSurveyManagerImpl: SurveyManager, Manager {
         }
     }
 
-    func sendEvent(with name: String, andProperties properies: [String : String]) {
-        survicate?.invokeEvent(name: name, with: properies)
+    func sendEvent(with name: String, andProperties properies: [String : Any]?) {
+        
+        if isPageView(name) {
+            viewScreen(with: name)
+        } else {
+            let stringOnlyProperties = properies?.compactMapValues { $0 as? String }
+            guard let stringOnlyProperties else { return }
+
+            survicate?.invokeEvent(name: name, with: stringOnlyProperties)
+        }
     }
 
     func viewScreen(with name: String) {
@@ -38,13 +52,32 @@ class KovaleeSurveyManagerImpl: SurveyManager, Manager {
         let userTrait = UserTrait(withName: name, value: value)
         survicate?.setUserTrait(userTrait)
     }
+
+    func setAmplitudeUserId(userId: String) {
+        let amplitudeIdUserTrait = UserTrait(withName: Constants.amplitudeUserIdKey, value: userId)
+        survicate?.setUserTrait(amplitudeIdUserTrait)
+    }
+
+    func setSurveyDelegate(_ delegate: KovaleeSurveyDelegate) {
+        self.delegate = delegate
+    }
 }
 
-// TODO: - Implement methods if needed (and remove unused)
+private extension KovaleeSurveyManagerImpl {
+
+    func isPageView(_ eventName: String) -> Bool {
+        return eventName.contains(Constants.pageViewEventNamePrefix)
+    }
+}
+
 extension KovaleeSurveyManagerImpl: SurvicateDelegate {
-    func surveyClosed(surveyId: String) {}
-    func surveyDisplayed(event: SurveyDisplayedEvent) {}
-    func questionAnswered(_ event: QuestionAnsweredEvent) {}
-    func surveyCompleted(event: SurveyCompletedEvent) {}
-    func surveyClosed(event: SurveyClosedEvent) {}
+    func surveyClosed(surveyId: String) {
+        delegate?.surveyClosed(surveyId: surveyId)
+    }
+    func surveyCompleted(event: SurveyCompletedEvent) {
+        delegate?.surveyCompleted(surveyId: event.surveyId)
+    }
+    func surveyClosed(event: SurveyClosedEvent) {
+        delegate?.surveyClosed(surveyId: event.surveyId)
+    }
 }
