d647e80aea7e5ec046b068e0e2371ab54699917f
fto-k <fto@kovalee.app>
2024-10-18 14:14:24 +0200

    1.12.3 release

--- Sources/KovaleeSDK/Kovalee.swift
+++ Sources/KovaleeSDK/Kovalee.swift
@@ -1,7 +1,7 @@
 import Foundation
 import KovaleeFramework
 
-public let SDK_VERSION = "1.12.2"
+public let SDK_VERSION = "1.12.3"
 
 /// A wrapper around all the third party tools used by Kovalee to gather information within the apps
 ///
