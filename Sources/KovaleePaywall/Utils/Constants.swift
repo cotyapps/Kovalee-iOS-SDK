ea003917342041233fc6f8fb893c8b9a95e08a83
ElMoez <e.amira@kovalee.app>
2025-01-10 14:23:43 +0100

    Fix: Remove KovaleePaywall

--- Sources/KovaleePaywall/Utils/Constants.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension String {
-    public static let isUserPremium = "k_is_user_premium"
-    static let paywallSource = "k_paywall_source"
-
-    static let paywallTestRunning = "paywall_test_running"
-}
