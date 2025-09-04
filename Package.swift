00629ac83ade901c2a9eab8f2c09c2cbb11a6d50
Stepan Kurennykh <stoope@outlook.com>
2025-09-04 13:53:20 +0200

    Update Firebase iOS SDK to 12.0.0

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/amplitude/Amplitude-Swift", .upToNextMajor(from: "1.4.3")),
         .package(url: "https://github.com/Survicate/survicate-ios-sdk", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "11.0.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "12.0.0")),
         .package(url: "https://github.com/RevenueCat/purchases-ios-spm.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/adjust/ios_sdk", .upToNextMajor(from: "5.0.0")),
     ],
