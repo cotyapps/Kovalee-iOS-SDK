d17f2bdc34531756c7b25ff8a8f2493bb4c0ed2a
Stepan Kurennykh <stoope@outlook.com>
2026-03-15 12:55:39 +0100

    Add multi-platform support

--- Sources/KovaleeRemoteConfig/FirebaseWrapperImpl.swift
+++ Sources/KovaleeRemoteConfig/FirebaseWrapperImpl.swift
@@ -2,147 +2,99 @@
 import Foundation
 import KovaleeFramework
 import KovaleeSDK
-
-#if canImport(FirebaseCore) && canImport(FirebaseRemoteConfig) && canImport(FirebaseAnalytics)
-    import FirebaseAnalytics
+#if canImport(FirebaseCore)
     import FirebaseCore
-    import FirebaseRemoteConfig
-
-    struct FirebaseWrapperImpl: RemoteConfigurationManager, Manager {
-        init(keys: KovaleeKeys.Firebase) {
-            if !keys.configuredInApp {
-                FirebaseApp.configure()
-            }
-        }
-
-        func setFetchTimeout(_ timeout: Double) {
-            RemoteConfig.remoteConfig().configSettings.fetchTimeout = timeout
-        }
-
-        func setDataCollectionEnabled(_ enabled: Bool) {
-            Analytics.setAnalyticsCollectionEnabled(enabled)
-        }
-
-        func setDefaultValues(_ values: [String: Any]) {
-            RemoteConfig.remoteConfig().setDefaults(values as? [String: NSObject])
-        }
-
-        func fetchAndActivateRemoteConfig() async {
-            do {
-                let remoteConfig = RemoteConfig.remoteConfig()
-                try await remoteConfig.ensureInitialized()
-
-                let activated = try await remoteConfig.fetchAndActivate()
-                KLogger.debug("🛰️ Remote config activated: \(activated)")
-                if activated == RemoteConfigFetchAndActivateStatus.error {
-                    throw KovaleeError.remoteValueFetchError
-                }
-
-                let keys = remoteConfig.allKeys(from: RemoteConfigSource.remote)
-                KLogger.debug("🛰️ Found remote config keys: [\(keys.joined(separator: ","))]")
-            } catch {
-                KLogger.error("❌ Got an error fetching remote values \(error)")
-            }
-        }
-
-        func value(forKey key: String) async throws -> Data {
-            await fetchAndActivateRemoteConfig()
-            KLogger.debug("🛰️ initialization complete")
-
-            return RemoteConfig.remoteConfig().configValue(forKey: key).dataValue
-        }
-    }
-
-    /// This class provides a wrapper for Remote Config parameter values, with methods to get parameter
-    /// values as different data types.
-    public class RemoteConfigValue {
-        /// Gets the value as a string.
-        public var value: String?
-
-        /// Gets the value as a Data object.
-        public var dataValue: Data
-
-        /// Gets the value as a number value.
-        public var numberValue: Double? {
-            guard let value else {
-                return nil
-            }
-            return Double(value)
-        }
-
-        /// Gets the value as a boolean.
-        public var boolValue: Bool? {
-            guard let value else {
-                return nil
-            }
-
-            return value.boolValue
-        }
-
-        init(data: Data) {
-            value = String(data: data, encoding: .utf8)
-            dataValue = data
-        }
-
-        init(config: FirebaseRemoteConfig.RemoteConfigValue) {
-            value = String(data: config.dataValue, encoding: .utf8)
-            dataValue = config.dataValue
-        }
-    }
-#else
-    struct FirebaseWrapperImpl: RemoteConfigurationManager, Manager {
-        init(keys: KovaleeKeys.Firebase) {
-        }
-
-        func setFetchTimeout(_ timeout: Double) {
-        }
-        
-        func fetchAndActivateRemoteConfig() async {
-        }
-        
-        func setDefaultValues(_ values: [String : Any]) {
-        }
-        
-        func value(forKey key: String) async throws -> Data {
-            Data()
-        }
-        
-        func setDataCollectionEnabled(_ enabled: Bool) {
-        }
-    }
-
-    /// This class provides a wrapper for Remote Config parameter values, with methods to get parameter
-    /// values as different data types.
-    public class RemoteConfigValue {
-        /// Gets the value as a string.
-        public var value: String?
-
-        /// Gets the value as a Data object.
-        public var dataValue: Data
-
-        /// Gets the value as a number value.
-        public var numberValue: Double? {
-            guard let value else {
-                return nil
-            }
-            return Double(value)
-        }
-
-        /// Gets the value as a boolean.
-        public var boolValue: Bool? {
-            guard let value else {
-                return nil
-            }
-
-            return value.boolValue
-        }
-
-        init(data: Data) {
-            value = String(data: data, encoding: .utf8)
-            dataValue = data
-        }
-    }
 #endif
+#if canImport(FirebaseRemoteConfig)
+    import FirebaseRemoteConfig
+#endif
+#if canImport(FirebaseAnalytics)
+    import FirebaseAnalytics
+#endif
+
+struct FirebaseWrapperImpl: RemoteConfigurationManager, Manager {
+    init(keys: KovaleeKeys.Firebase) {
+        if !keys.configuredInApp {
+            FirebaseApp.configure()
+        }
+    }
+
+    func setFetchTimeout(_ timeout: Double) {
+        RemoteConfig.remoteConfig().configSettings.fetchTimeout = timeout
+    }
+
+    func setDataCollectionEnabled(_ enabled: Bool) {
+        #if canImport(FirebaseAnalytics)
+            Analytics.setAnalyticsCollectionEnabled(enabled)
+        #endif
+    }
+
+    func setDefaultValues(_ values: [String: Any]) {
+        RemoteConfig.remoteConfig().setDefaults(values as? [String: NSObject])
+    }
+
+    func fetchAndActivateRemoteConfig() async {
+        do {
+            let remoteConfig = RemoteConfig.remoteConfig()
+            try await remoteConfig.ensureInitialized()
+
+            let activated = try await remoteConfig.fetchAndActivate()
+            KLogger.debug("🛰️ Remote config activated: \(activated)")
+            if activated == RemoteConfigFetchAndActivateStatus.error {
+                throw KovaleeError.remoteValueFetchError
+            }
+
+            let keys = remoteConfig.allKeys(from: RemoteConfigSource.remote)
+            KLogger.debug("🛰️ Found remote config keys: [\(keys.joined(separator: ","))]")
+        } catch {
+            KLogger.error("❌ Got an error fetching remote values \(error)")
+        }
+    }
+
+    func value(forKey key: String) async throws -> Data {
+        await fetchAndActivateRemoteConfig()
+        KLogger.debug("🛰️ initialization complete")
+
+        return RemoteConfig.remoteConfig().configValue(forKey: key).dataValue
+    }
+}
+
+/// This class provides a wrapper for Remote Config parameter values, with methods to get parameter
+/// values as different data types.
+public class RemoteConfigValue {
+    /// Gets the value as a string.
+    public var value: String?
+
+    /// Gets the value as a Data object.
+    public var dataValue: Data
+
+    /// Gets the value as a number value.
+    public var numberValue: Double? {
+        guard let value else {
+            return nil
+        }
+        return Double(value)
+    }
+
+    /// Gets the value as a boolean.
+    public var boolValue: Bool? {
+        guard let value else {
+            return nil
+        }
+
+        return value.boolValue
+    }
+
+    init(data: Data) {
+        value = String(data: data, encoding: .utf8)
+        dataValue = data
+    }
+
+    init(config: FirebaseRemoteConfig.RemoteConfigValue) {
+        value = String(data: config.dataValue, encoding: .utf8)
+        dataValue = config.dataValue
+    }
+}
 
 extension String {
     var boolValue: Bool {
