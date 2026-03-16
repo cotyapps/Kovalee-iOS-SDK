e817ebcb87afd61126fc6025412a2dd11c2ffc2b
Stepan Kurennykh <stoope@outlook.com>
2026-03-16 20:19:14 +0100

    Add support for other platforms

--- Sources/KovaleeSDK/Amplitude/AmplitudeWrapperImpl.swift
+++ Sources/KovaleeSDK/Amplitude/AmplitudeWrapperImpl.swift
@@ -32,7 +32,7 @@ import KovaleeFramework
             
             #if canImport(AmplitudeSwiftSessionReplayPlugin)
                 if sessionReplayPluginEnabled {
-                    amplitude.withAmplitude { $0?.add(plugin: AmplitudeSwiftSessionReplayPlugin(sampleRate: 1.0)) }
+                    amplitude.withAmplitude { $0?.add(plugin: AmplitudeSwiftSessionReplayPlugin()) }
                 }
             #endif
 
